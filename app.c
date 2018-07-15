b1844570eafaacd15ec7d2a4c0964cf59279bf70
BeentageBand <josera.baez@gmail.com>
2018-07-15 17:48:41 -0500

    fixes on app

--- app.c
+++ app.c
@@ -11,10 +11,19 @@
 
 #define Dbg_FID DBG_FID_DEF(APP_FID, 0)
 #define COBJECT_IMPLEMENTATION
+#include "app.h"
+#include "app_fsm.h"
 #include "dbg_log.h"
 #include "ipc.h"
-#include "app.h"
-#include "app_ext.h"
+
+/*==============================================================================
+ * Local Types
+ * ============================================================================*/
+struct APP_FSM
+{
+      union Worker * worker;
+      union FSM fsm;
+};
 
 /*==============================================================================
  * Local Prototypes
@@ -24,7 +33,7 @@ static void application_on_mail(union Worker * const super, union Mail * const m
 static void application_on_start(union Worker * const super);
 static void application_on_loop(union Worker * const super);
 static void application_on_stop(union Worker * const super);
-static void application_startup(union Application * const this);
+static int application_startup(union Application * const this);
 
 /*==============================================================================
  * Local Objects 
@@ -34,67 +43,90 @@ Application_Class_T Application_Class =
       {
             {application_delete, NULL},
             application_on_mail,
-            application_start, 
-            application_loop,  
-            application_stop
+            application_on_start, 
+            application_on_loop,  
+            application_on_stop
       },
       application_startup
 }};
 
 static union Application Application = {{NULL}};
-static union Mailbox Application_Mailbox = {{NULL}};
 static union Mail Application_Mail_Buff[64] ={0};
-static union FSM Application_Pool[IPC_MAX_TID] = {0};
+static struct APP_FSM Application_Pool[IPC_MAX_TID] = {0};
+static union St_Machine_State Application_St_Machine[APP_MAX_STID];
 
 /*==============================================================================
  * Local Functions
  * ============================================================================*/
 void application_delete(struct Object * const obj)
 {
-   Application_T * const this = _cast(Application, obj);
+   union Application * const this = _cast(Application, obj);
    Isnt_Nullptr(this, );
 }
 
 void application_on_mail(union Worker * const super, union Mail * const mail)
-{
-
-}
-
-void application_on_start(union Worker * const super)
-{
-   Application_T * const this = _cast(Application, super);
-   Isnt_Nullptr(this, );
-}
-
-void application_on_loop(union Worker * const super)
 {
    Application_T * const this = _dynamic_cast(Application, super);
    Isnt_Nullptr(this, );
    Mail_T const mail = {{NULL}};
    if (IPC_retreive_mail(&mail, IPC_RETRIEVE_TOUT_MS))
    {
-      if(mail.tid > APP_START_THREADS_INT_MID)
+         if(mail.mid >= APP_START_THREADS_INT_MID &&
+            mail.mid <= APP_SHUTDOWN_INT_MID)
+         {
+               Isnt_Nullptr(mail.payload, );
+               IPC_TID_T tid = *(IPC_TID_T *) mail.payload;
+               Application_Pool[tid].fsm->dispatch(Application_Pool + tid, &mail);
+         }
+   }
+}
+
+void application_on_start(union Worker * const super)
+{
+   union Application * const this = _cast(Application, super);
+   Isnt_Nullptr(this, );
+   IPC_TID_T i;
+   for(i = 0; i < IPC_MAX_TID; ++i)
+   {
+         union Worker * worker = Application_Pool[i].worker;
+         if(worker)
+         {
+               IPC_Self_Send(APP_START_THREADS_INT_MID, &i, sizeof(i));
+         }
+   }
+}
+
+void application_on_loop(union Worker * const super)
+{
+      bool is_alive = true;
+      IPC_TID_T i;
+      for(i = 0; i < IPC_MAX_TID; ++i)
       {
-            /* */
+            if(Application_Pool[i].worker &&
+            APP_TERM_STID != Application_Pool[i].fsm.State_Machine.current_st)
+            {
+                  is_alive = false;
+                  break;
+            }
       }
 
-   }
+      if(is_alive)
+      {
+            IPC_Self_Send(WORKER_SHUTDOWN_INT_MID, NULL, 0);
+      }
 }
 
 void application_on_stop(union Worker * const super)
 {
-   Application_T * const this = _dynamic_cast(Application, super);
+   union Application * const this = _cast(Application, super);
    Isnt_Nullptr(this, );
-   Hama_HSM_Event_T ev =
-   {
-         0, NULL, 0 /* FIXME terminal */
-   };
-   this->fsm.vtbl->dispatch(&this->fsm, &ev);
 }
 
-void application_startup(union Application * const this)
+int application_startup(union Application * const this)
 {
       IPC_Run(APP_WORKER_TID);
+      while(1){}
+      return -1; //TERMINATED
 }
 
 /*==============================================================================
@@ -104,13 +136,11 @@ void Populate_Application(union Application * const this, union Worker * (* fact
 {
       if(NULL == Application.vtbl)
       {
-            Populate_Worker(&Application.Worker, APP_WORKER_TID, &Application_Mailbox, 
+            Populate_Worker(&Application.Worker, APP_WORKER_TID,
             Application_Mail_Buff, Num_Elems(Application_Mail_Buff));
             Object_Init(&Application.Object, &Application_Class.Class,
             sizeof(Application_Class.Worker));
             Application.vtbl = &Application_Class;
-            Application.fsm = Application_Pool;
-            Application.fsm = Num_Elems(Application_Pool)
       }
       memcpy(this, &Application, sizeof(Application));
 
@@ -127,26 +157,20 @@ void Populate_Application(union Application * const this, union Worker * (* fact
       }
 }
 
-void Application_run_all_apps(void)
-{
-   Application_T * this = NULL;
-   Application_get_instance(&this);
-   Isnt_Nullptr(this,);
-   this->Worker.Task.vtbl->run(&this->Worker.Task);
-
-}
-
 void Application_initialized(void)
 {
-   IPC_send(APP_WORKER_TID,  APP_TASK_INIT_TID, NULL, 0);
+   IPC_TID_T self = IPC_Self();
+   IPC_Send(APP_WORKER_TID,  APP_THREAD_INIT_INT_MID, &self, sizeof(self));
 }
 
 void Application_terminated(void)
 {
-   IPC_send(APP_WORKER_TID,  APP_TASK_TERM_TID, NULL, 0);
+   IPC_TID_T self = IPC_Self();
+   IPC_Send(APP_WORKER_TID,  APP_THREAD_TERM_INT_MID, &self, sizeof(self));
 }
 
 void Application_shutdown(void)
 {
-   IPC_send(APP_WORKER_TID,  APP_SHUTDOWN_TID, NULL, 0);
+   IPC_TID_T self = IPC_Self();
+   IPC_Send(APP_WORKER_TID,  APP_SHUTDOWN_INT_MID, &self, sizeof(self));
 }
--- app.h
+++ app.h
@@ -22,19 +22,14 @@ typedef union Application
 {
     union Application_Class _private * _private vtbl;
     struct Object Object;
-    struct
-    {
-        union Worker Worker;
-        union FSM _private * _private fsm;
-        size_t _private n_fsm;
-    };
+    union Worker Worker;
 }Application_T;
 
 typedef union Application_Class
 {
     struct
     {
-        union Worker;
+        union Worker_Class Worker;
         int (* _private startup)(union Application * const);
     };
     struct Class Class;
@@ -42,7 +37,7 @@ typedef union Application_Class
 
 extern Application_Class_T _private Application_Class;
 
-extern void Populate_Application(union Application * const app);
+extern void Populate_Application(union Application * const this, union Worker * (* factory_method)(IPC_TID_T const tid));
 extern void Application_initialized(void);
 extern void Application_terminated(void);
 extern void Application_shutdown(void);
@@ -50,3 +45,5 @@ extern void Application_shutdown(void);
 #ifdef __cplusplus
 }
 #endif
+
+#endif /*APP_H_*/
